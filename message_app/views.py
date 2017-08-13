f0c484e9e870f9ca3b04d4b435b5d7f51b33eff4
Koffi Sani <koffisani@gmail.com>
2017-08-13 21:35:35 +0100

    Working on message board app

--- message_app/views.py
+++ message_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from .models import Post
+
+class Home_page_view(ListView):
+    model = Post
+    template_name = 'message_app/index.html'
+
